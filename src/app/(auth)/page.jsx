c1eb9290e73c807ca09841fd731d5584d01c8293
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2024-03-02 21:52:43 +0530

    remove junk code

--- src/app/(auth)/page.jsx
+++ src/app/(auth)/page.jsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import Form from '../../components/Form/Form'
-=======
 
->>>>>>> 46f8a00227de28388c76e6a608e52449a4056b88
-import React from 'react'
-import Form from '../../components/Form/Form'
+import React from "react";
+import Form from "../../components/Form/Form";
 
 const Login = () => {
-  return (
-    <Form type={"login"}/>
-    
-  )
-}
+  return <Form type={"login"} />;
+};
 
-export default Login
+export default Login;
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import Form from '../../../components/Form/Form'
-
-=======
-import Form from "../../../components/Form/Form";
->>>>>>> 46f8a00227de28388c76e6a608e52449a4056b88
 import React from "react";
+import Form from "../../../components/Form/Form";
 
 
 const Register = () => {
