8db17fcbd6792f2d66dc24b881cde46788896551
Rolex <himanshugola1111@gmail.com>
2024-03-03 22:40:48 +0530

    wrapping up

--- src/app/(root)/chats/page.jsx
+++ src/app/(root)/chats/page.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+"use client";
+import { useSession } from "next-auth/react";
 
 const page = () => {
-  return <div>welcome to chat</div>;
+  const { data: session } = useSession();
+  console.log("====================================");
+  console.log("auth data", session);
+  console.log("====================================");
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+
+      <h1>hyyy</h1>
+    </div>
+  );
 };
 
 export default page;
