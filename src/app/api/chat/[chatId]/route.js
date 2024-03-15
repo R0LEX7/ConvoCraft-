bf1ad0bf527878b389044606a34c8da7695850f5
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2024-03-15 18:18:40 +0530

    imp chat details endpoint

---
+++ src/app/api/chat/[chatId]/route.js
@@ -0,0 +1,25 @@
+import { NextResponse, NextRequest } from "next/server";
+import { connect } from "../../../../dbConfig/dbConfig";
+import User from "../../../../models/user.model";
+import Chat from "../../../../models/chat.model";
+
+export async function GET(req, { params }) {
+  try {
+    const { chatId } = params;
+    const chat = await Chat.findById(chatId).populate({
+      path: "members",
+      model: User,
+    });
+    return NextResponse.json({ success: true, data: chat }, { status: 200 });
+  } catch (error) {
+    console.log("error in getting chat details ", error.message);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "error in getting chat details ",
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
