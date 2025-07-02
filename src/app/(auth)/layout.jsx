b96803880c74c6c5deb8cb664f4f43bc6d2484c8
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2025-07-02 21:54:23 +0530

    changed meta data

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -2,18 +2,22 @@
 
 import "../globals.css";
 import { Providers } from "../provider";
+import { Fira_Code, Work_Sans } from "next/font/google";
 
 
+const font = Work_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Auth Halo Chat",
-  description: "Build a Next 14 Chat App",
+  title: "ConvoCraft - Auth",
+  description: "Convocraft is a Nextjs full-stack chatting application ",
 };
 
 export default function RootLayout({ children }) {
+
+
   return (
     <html lang="en">
-      <body>
+     <body className={font.className}>
       <Providers>
       {children}
     </Providers>
